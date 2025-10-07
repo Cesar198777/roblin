0c580d52735423382dc1cc213c98e6740ab8b9a8
Cesar198777 <galvan.cesar1987@gmail.com>
2025-10-07 18:59:24 -0300

    Update probando.lua

--- probando.lua
+++ probando.lua
@@ -1,17 +1,17 @@
 repeat task.wait(.12345) until game:IsLoaded()
 
-if game.GameId == 7882829745 then
-    --[[ 
+if  game.gameId == 7882829745 then
+    --[[
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
     ]]
-
+   
     local vu = game:GetService("VirtualUser")
     game:GetService("Players").LocalPlayer.Idled:connect(function()
-        vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+        vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
         task.wait(1)
-        vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+        vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
     end)
-
+    
     task.spawn(function()
         local VirtualInputManager = game:GetService("VirtualInputManager")
         local X, Y = 111, 960
@@ -24,19 +24,23 @@ if game.GameId == 7882829745 then
             task.wait(600)
         end
     end)
-
+    
 else
-    --[[ 
+    
+    --[[
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/180154ec7abc246752b60603a7adc12d.lua"))()
     ]]
 
+    local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/customLua/main/SimpleUiLib.lua"))()
+    ui.CreateBHUBPAID("")
+    
     local vu = game:GetService("VirtualUser")
     game:GetService("Players").LocalPlayer.Idled:connect(function()
-        vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+        vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
         task.wait(1)
-        vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+        vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
     end)
-
+    
     task.spawn(function()
         local VirtualInputManager = game:GetService("VirtualInputManager")
         local X, Y = 111, 960
@@ -49,4 +53,6 @@ else
             task.wait(600)
         end
     end)
+    
 end
+
